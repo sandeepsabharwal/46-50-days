1ec0fb9b471f11f55ac7a6fbf72da029c4708b09
Sandeep Sabharwal <sabharwalsandeep989@gmail.com>
2025-11-05 22:20:07 +0530

    Create day47 que1.c

---
+++ day47 que1.c
@@ -0,0 +1,27 @@
+# Check if two strings are anagrams of each other.
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int freq[26] = {0};
+    int i = 0;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    while(str[i] != '\0') {
+        if(str[i] >= 'a' && str[i] <= 'z') {
+            int index = str[i] - 'a';
+            freq[index]++;
+            if(freq[index] == 2) {
+                printf("First repeating lowercase alphabet: %c\n", str[i]);
+                return 0;
+            }
+        }
+        i++;
+    }
+
+    printf("No repeating lowercase alphabet found\n");
+    return 0;
+}
