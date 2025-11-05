b67810f4a63567ddcc466cef14606e212e882222
Sandeep Sabharwal <sabharwalsandeep989@gmail.com>
2025-11-05 22:23:51 +0530

    Create day49 que1.c

---
+++ day49 que1.c
@@ -0,0 +1,23 @@
+# Print the initials of a name.
+
+  #include <stdio.h>
+
+int main() {
+    char name[100];
+    int i = 0;
+
+    printf("Enter a full name: ");
+    scanf(" %[^\n]", name);
+
+    if(name[0] != ' ')
+        printf("%c", name[0]);
+
+    while(name[i] != '\0') {
+        if(name[i] == ' ' && name[i + 1] != ' ' && name[i + 1] != '\0')
+            printf("%c", name[i + 1]);
+        i++;
+    }
+
+    printf("\n");
+    return 0;
+}
