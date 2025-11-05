ce6765b9519461fdaac61431c82bb7372afd31db
Sandeep Sabharwal <sabharwalsandeep989@gmail.com>
2025-11-05 22:21:58 +0530

    Create day48 que1.c

---
+++ day48 que1.c
@@ -0,0 +1,29 @@
+# Check if one string is a rotation of another.
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[100], str2[100], temp[200];
+
+    printf("Enter first string: ");
+    scanf("%s", str1);
+
+    printf("Enter second string: ");
+    scanf("%s", str2);
+
+    if(strlen(str1) != strlen(str2)) {
+        printf("Not a rotation\n");
+        return 0;
+    }
+
+    strcpy(temp, str1);
+    strcat(temp, str1);
+
+    if(strstr(temp, str2))
+        printf("Yes, it's a rotation\n");
+    else
+        printf("No, it's not a rotation\n");
+
+    return 0;
+}
